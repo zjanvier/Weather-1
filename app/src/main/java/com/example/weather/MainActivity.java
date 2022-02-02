10eac67fc3480787770761bbfcfd1211740fa8bf
Lauren Kwong <laurenkwong@Laurens-MacBook-Pro.local>
2022-02-01 21:50:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    // déclaration des champs
+    TextView mDate,mCity,mTemp,mDescription;
+    ImageView imgIcon;
+    String maVille="Toronto";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDate=findViewById(R.id.mDate);
+        mCity=findViewById(R.id.mCity);
+        mTemp=findViewById(R.id.mTemp);
+        mDescription=findViewById(R.id.mDescription);
+    }
+
+    public void afficher()
+    {
+        String url="http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=edaabd12e54121169539140cbc057801&units=metric";
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                try {
+                    JSONObject main_object=response.getJSONObject("main");
+                    JSONArray array = response.getJSONArray("weather");
+                    Log.d("Tag", "resultat = "+array.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+}
